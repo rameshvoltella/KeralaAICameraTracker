4a63829dea1624ac4cb4827e10a371f7e7d05a2d
Ramesh M Nair <ramesh.nair@oliveboard.in>
2024-03-22 16:15:14 +0530

    added some layout play with compose

--- app/src/main/java/com/ramzmania/aicammvd/ui/home/MainActivity.kt
+++ app/src/main/java/com/ramzmania/aicammvd/ui/home/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ramzmania.aicammvd.ui.compose.slider.Greeting
 import com.ramzmania.aicammvd.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,13 +30,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/ramzmania/aicammvd/ui/slider/SliderActivity.kt
+++ app/src/main/java/com/ramzmania/aicammvd/ui/slider/SliderActivity.kt
@@ -1,12 +1,16 @@
 package com.ramzmania.aicammvd.ui.slider
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ramzmania.aicammvd.R
 import com.ramzmania.aicammvd.ui.base.BaseComposeActivity
-import com.ramzmania.aicammvd.ui.home.Greeting
+import com.ramzmania.aicammvd.ui.compose.slider.ComposeArticle
+import com.ramzmania.aicammvd.ui.compose.slider.Greeting
 import com.ramzmania.aicammvd.ui.theme.MyApplicationTheme
 import com.ramzmania.aicammvd.viewmodel.slider.SliderViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,8 +33,22 @@ class SliderActivity : BaseComposeActivity<SliderViewModel>() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background // Updated MaterialTheme.colorScheme to MaterialTheme.colors
             ) {
-                Greeting("Androidolakka")
+                Column {
+                    Greeting("Androidsherikkum")
+                    ComposeArticle("Rajavu","dfbljwebfjbwejbfjhbefjhw","body2",R.drawable.ic_livevideo_doubt)
+                }
+                //Greeting("Androidsherikkum")
             }
+        }
+    }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        Column {
+            Greeting("Androidsherikkum")
+            ComposeArticle("Rajavu","dfbljwebfjbwejbfjhbefjhw","body2", R.drawable.ic_livevideo_doubt)
         }
     }
 }
