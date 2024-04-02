d94a6485dfe28f25c894d2cd29227100590c76e5
Ramesh M Nair <ramesh.nair@oliveboard.in>
2024-04-02 11:50:53 +0530

    removed un used import

--- app/src/main/java/com/ramzmania/aicammvd/ui/component/cameralist/CameraLazyList.kt
+++ app/src/main/java/com/ramzmania/aicammvd/ui/component/cameralist/CameraLazyList.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.items
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import com.ramzmania.aicammvd.R
 
