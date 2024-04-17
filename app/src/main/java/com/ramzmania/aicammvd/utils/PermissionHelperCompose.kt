8e10aa1cea029cd3bcf3eed941c5a72b04855d1b
Ramesh M Nair <ramesh.nair@oliveboard.in>
2024-04-17 11:44:33 +0530

    added proper permission navigation iver denaying permission

--- app/src/main/java/com/ramzmania/aicammvd/utils/PermissionHelperCompose.kt
+++ app/src/main/java/com/ramzmania/aicammvd/utils/PermissionHelperCompose.kt
@@ -1,21 +1,31 @@
 package com.ramzmania.aicammvd.utils
 
 
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.permissions.shouldShowRationale
+import com.ramzmania.aicammvd.R
 
 @ExperimentalPermissionsApi
 @Composable
@@ -24,7 +34,7 @@ fun PermissionsHandler(
     onPermissionsGranted: () -> Unit
 ) {
     val permissionsState = rememberMultiplePermissionsState(permissions = permissions)
-
+    val context = LocalContext.current
 
     val lifecycleOwner = LocalLifecycleOwner.current
     DisposableEffect(key1 = lifecycleOwner) {
@@ -51,13 +61,47 @@ fun PermissionsHandler(
                 perm.status.isGranted -> {
                     Text(text = "${perm.permission} permission accepted")
                 }
+
                 perm.status.shouldShowRationale -> {
-                    Text(text = "${perm.permission} permission is needed to use this feature")
+//                    Text(text = "${perm.permission} permission is needed to use this feature")
+                    ClickableText(
+                        text = AnnotatedString("App wont work without location permission go to setting and enable location permission+\n Select AI Camera app -> Allow all the time"),
+                        onClick = {
+                            val settingsIntent = Intent(
+                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            )
+                            settingsIntent.data = Uri.parse("package:" + context.packageName);
+
+                            context.startActivity(settingsIntent)
+                        },
+                        style = TextStyle(
+                            color = colorResource(
+                                id = R.color.white_perment
+                            ), fontSize = 16.sp
+                        )
+                    )
+
                     allPermissionsGranted = false
                 }
 
                 else -> {
-                    Text(text = "${perm.permission} permission is needed to use this feature")
+//                    Text(text = "${perm.permission} permission is needed to use this feature")
+                    ClickableText(
+                        text = AnnotatedString("App wont work without location permission go to setting and enable location permission+\n Select AI Camera app -> Allow all the time"),
+                        onClick = {
+                            val settingsIntent = Intent(
+                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            )
+                            settingsIntent.data = Uri.parse("package:" + context.packageName);
+                            context.startActivity(settingsIntent)
+                        },
+                        style = TextStyle(
+                            color = colorResource(
+                                id = R.color.white_perment
+                            ), fontSize = 16.sp
+                        )
+                    )
+
                     allPermissionsGranted = false
                 }
             }
